7b941b6cb73bb5f3eade83a1bcf194960decfb9c
swathij943 <swathij943@gmail.com>
2023-05-19 05:12:26 -0400

    Assignment on Cats as Service

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,28 +1,58 @@
-//import 'request' mtd that allow us to make a HTTP requests
-//using process.argv to retrive breed name from the command-line arguments
-//construct the API url
-//making a HTTP request - request fn calls url & provides a cb to handle the response
-//when the 'request' fn calls the url - it sends a HTTP GET request to the specified url & provides a cb fn to handle response
-
+//Refactored code
 const request = require('request');
 
-const breedName = process.argv[2];
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-  } else if (response.statusCode !== 200) {
-    console.error('Invalid response status code:', response.statusCode);
-  } else {
-    const data = JSON.parse(body); //response body parsed to JSON.parse
-    if (data.length === 0) {
-      console.log(`Breed '${breedName}' not found.`);
+  request(url, (error, response, body) => {
+    if(error) {
+      callback(error, null);
+    } else if (response.statusCode !== 200) {
+      callback(`Invalid response status code: ${response.statusCode}`, null);
     } else {
-      const breed = data[0];
-      console.log('Description:', breed.description);
+      const data = JSON.parse(body);
+      if (data.length === 0) {
+        callback(`Breed '${breedName}' not found.`, null);
+      } else {
+        const breed = data[0];
+        callback(null, breed.description);
+      }
     }
-  }
-});
+  });
+};
+
+module.exports = { fetchBreedDescription };
+
+
+
+
+
+
+// //import 'request' mtd that allow us to make a HTTP requests
+// //using process.argv to retrive breed name from the command-line arguments
+// //construct the API url
+// //making a HTTP request - request fn calls url & provides a cb to handle the response
+// //when the 'request' fn calls the url - it sends a HTTP GET request to the specified url & provides a cb fn to handle response
+
+// const request = require('request');
+
+// const breedName = process.argv[2];
+
+// const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+
+// request(url, (error, response, body) => {
+//   if (error) {
+//     console.error('Error:', error);
+//   } else if (response.statusCode !== 200) {
+//     console.error('Invalid response status code:', response.statusCode);
+//   } else {
+//     const data = JSON.parse(body); //response body parsed to JSON.parse
+//     if (data.length === 0) {
+//       console.log(`Breed '${breedName}' not found.`);
+//     } else {
+//       const breed = data[0];
+//       console.log('Description:', breed.description);
+//     }
+//   }
+// });
 
